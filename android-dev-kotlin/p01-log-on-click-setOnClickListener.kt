84ed3547f115caa25a888cad3239369767739e22
SP <spgithubacc@gmail.com>
2021-12-01 13:40:46 +0530

    2021-12-01-13-40

---
+++ android-dev-kotlin/p01-log-on-click-setOnClickListener.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mb = findViewById<Button>(R.id.mybutton);
+        mb.setOnClickListener {
+            Log.d("Log Tag", "Log Message")
+        }
+    }
+}

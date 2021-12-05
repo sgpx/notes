f303bd2f734fd2272a4c09289316f151def3e0e7
SP <spgithubacc@gmail.com>
2021-12-05 09:04:00 +0530

    2021-12-05-09-04

--- android-dev-notes/android-notes-2/a.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-a="app/src/main/res/values/colors.xml"
-cat $a | sed -r "s/<\/resources>//" > tmp.xml;
-#echo '<color name="white">#FFFFFF</color>' >> tmp.xml
-printf '\t<color name="white">#FFFFFF</color>' >> tmp.xml
-printf '\n</resources>' >> tmp.xml
-mv -v tmp.xml $a

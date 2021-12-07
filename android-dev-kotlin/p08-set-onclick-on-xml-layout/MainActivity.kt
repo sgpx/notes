09b49ea6dd7273899990f45cf23aeaf598f3a212
SP <spgithubacc@gmail.com>
2021-12-07 12:44:13 +0530

    2021-12-07-12-44

---
+++ android-dev-kotlin/p08-set-onclick-on-xml-layout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.app1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun myFunc(view : View){
+        Toast.makeText(this, "myFunc triggered", Toast.LENGTH_SHORT).show()
+    }
+}

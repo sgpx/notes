5d240a8b7871e04508594bc8a92ad33951e75cb5
SP <spgithubacc@gmail.com>
2021-12-14 18:14:25 +0530

    2021-12-14-18-14

---
+++ knr-c/p47.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+	int x = 1;
+	int y = 2;
+	int z = y | x;
+	printf("%d\n",z);
+
+}
+
+
+// output
+// 3
---
+++ knr-c/p48.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main()
+{
+	int x = 3;
+	int y = 2;
+	int z = y ^ x;
+	printf("%d\n",z);
+
+}
+
+
+// output
+// 3
+
+
+// output
+// 3
+
+
+// output
+// 1

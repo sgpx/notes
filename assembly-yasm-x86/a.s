4efcd8d5abae2b0504c0bc8648a4fa5c87c7dbb3
sgpx <spgithubacc@gmail.com>
2022-02-20 21:50:23 +0530

    2022-02-20-21-50

--- assembly-yasm-x86/a.s
+++ assembly-yasm-x86/a.s
@@ -1,16 +1,7 @@
-section .text
 global _start
+
 _start:
-	mov rax, 12
-	cmp rax, 12
-	je lol
-	; if RAX equals 12 then jump to "lol"
+	rdrand rdi
 
 	mov rax, 60
-	mov rdi, 0
-	syscall
-
-lol:
-	mov rax, 60
-	mov rdi, 1
 	syscall

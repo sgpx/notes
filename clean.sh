6406ed17708e55ad06f2b4a97bd43733f3560177
sgpx <spgithubacc@gmail.com>
2022-02-26 14:19:25 +0530

    2022-02-26-14-19

--- clean.sh
+++ clean.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-for i in $(find . -iname "*.class" -depth); do
-	echo removing $i;
-	rm -v $i;
-done;
-for i in $(find . -iname "*.jar" -depth); do
-	echo removing $i;
-	rm -v $i;
-done;
+function cleanup()
+{
+	echo cleaning ".$1" files;
+	for i in $(find . -iname "*.$1" -depth); do
+		echo removing $i;
+		rm -v $i;
+	done;
+}
+
+cleanup jar
+cleanup class
+cleanup out
+cleanup o
+cleanup obj

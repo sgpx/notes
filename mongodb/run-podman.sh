a964a225d88e18ac2b979000326e98f126dc23a7
sgpx <spgithubacc@gmail.com>
2022-03-23 08:03:25 +0530

    2022-03-23-08-03

--- mongodb/run-podman.sh
+++ mongodb/run-podman.sh
@@ -15,14 +15,32 @@ fi
 opt=""
 dbdata_path=~/dbdata
 
-if [ "$1" = "-a" ]; then
+function set_opt(){
 	opt="--auth"
-fi
+}
 
-if [ "$1" = "-r" ]; then
+function clear_dbdata(){
 	rm -rf $dbdata_path
 	mkdir $dbdata_path
-fi
+}
+
+function process_arg(){
+	if [ "$1" = "a" ]; then
+		echo setting opt
+		set_opt;
+	fi
+
+	if [ "$1" = "r" ]; then
+		echo clearing dbdata
+		clear_dbdata;
+	fi
+}
+
+arg="$1"
+arg_len=${#arg}
+ctr=0
+
+while (( $ctr < $arg_len )); do process_arg "${arg:$ctr:1}"; ctr=$(expr $ctr + 1); done
 
 cd mdb/bin
 pwd

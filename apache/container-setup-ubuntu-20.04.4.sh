7cc0fa6c8c64f649f5a6dd89e033862b8fbc7aaf
sgpx <spgithubacc@gmail.com>
2022-05-20 10:37:15 +0530

    2022-05-20-10-37

--- apache/container-setup-ubuntu-20.04.4.sh
+++ apache/container-setup-ubuntu-20.04.4.sh
@@ -1,8 +1,7 @@
-# setup (container, without systemd)
-
-```
-apt install -y apache2
-sed -i .bak -r "s/Listen 80/Listen 5000/" /etc/apache2/ports.conf
+#!/bin/bash
+apt update
+apt install -y --allow-downgrades apache2 perl-base=5.30.0-9build1 perl
+sed -i.bak -r "s/Listen 80/Listen 5000/" /etc/apache2/ports.conf
 echo "ServerName 10.88.0.2" >> /etc/apache2/sites-available/000-default.conf
 
 useradd apacheuser
@@ -20,7 +19,4 @@ chown apacheuser /home/apacheuser/*
 
 cd /home/apacheuser
 
-su -c 'cd /home/apacheuser && APACHE_RUN_DIR="$PWD/apache-run-dir/" APACHE_RUN_USER="apacheuser" APACHE_RUN_GROUP="apachegroup" APACHE_LOG_DIR="$PWD/apache-log-dir/" APACHE_PID_FILE="$PWD/apache.pidfile" apache2' apacheuser 
-```
-
-
+su -c 'cd /home/apacheuser && APACHE_RUN_DIR="$PWD/apache-run-dir/" APACHE_RUN_USER="apacheuser" APACHE_RUN_GROUP="apachegroup" APACHE_LOG_DIR="$PWD/apache-log-dir/" APACHE_PID_FILE="$PWD/apache.pidfile" apache2' apacheuser

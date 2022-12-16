b887652a5b635a9930288be662f6947691b92196
sgpx <spgithubacc@gmail.com>
2022-12-17 00:57:01 +0530

    2022-12-17-00-57

--- mongodb/libcrypto.so.1.1-not-found-fix.sh
+++ mongodb/libcrypto.so.1.1-not-found-fix.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 platform=$(uname -i)
+chksum="c5ac01e760ee6ff0dab61d6b2bbd30146724d063eb322180c6f18a6f74e4b6aa"
+
+apt update
 apt install -y build-essential
-wget https://www.openssl.org/source/openssl-1.1.1o.tar.gz -O o.tgz
+
+wget https://www.openssl.org/source/openssl-1.1.1s.tar.gz -O o.tgz
+calcsum="$(shasum -a 256 o.tgz | sed -r 's/([a-z0-9]+).+/\1/')"
+
+if [ "$chksum" = "$calcsum" ]; then
+	echo checksum match OK
+else
+	echo checksum match failed, exiting..
+	exit 1
+fi
+
 tar xvzf o.tgz
-cd openssl-1.1.1o
+cd openssl-1.1.1s
 ./Configure linux-$platform
 make
 make install
---
+++ shasum/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+a="https://www.openssl.org/source/openssl-1.1.1s.tar.gz"
+wget $a
+wget $a.sha256
+chksum=$(shasum --algorithm 256 openssl-1.1.1s.tar.gz | sed -r "s/([a-z0-9]+).+/\1/")
+
+if [ "$chksum" = "$(cat openssl-1.1.1s.tar.gz.sha256)" ]; then
+	echo checksum match OK;
+else
+	echo checksum match fail;
+fi
+

2eec41d7577e95384d5e5437b38f66c3c994d56c
sgpx <spgithubacc@gmail.com>
2023-01-29 15:47:35 +0530

    2023-01-29-15-47

---
+++ knr-c/p148_remove_element_from_linked_list.c
@@ -0,0 +1,130 @@
+/*
+to remove last node
+
+1. node * a = base
+2. while(a->next != NULL) a = a->next;
+
+=====
+
+to remove first node
+1. node * a = base
+2. *a = *(a->next)
+
+[0x01]
+val : 25
+next : 0x02
+
+[0x02]
+val : 26
+next : 0x03
+
+[0x03]
+val : 27
+next : NULL
+
+----
+
+[0x01]
+val : 25
+next : 0x03
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node_struct {
+	int num;
+	struct node_struct *next;
+};
+
+typedef struct node_struct node;
+
+void assign_ll(node *ref){
+	node *p = ref;
+	for(int i = 101; i < 110; i++){
+		p->num = i;
+		p->next = malloc(sizeof(node));
+		p = p->next;
+	}
+	// avoid valgrind uninitialized errors
+	p->num = 110;
+	p->next = NULL;
+}
+
+void print_ll(node *ref){
+	node *p = ref;
+	while(p != NULL){
+		printf("%p %d\n", p, p->num);
+		p = p->next;
+	}
+}
+
+void free_ll(node *ref){
+	node *p = ref, *q;
+	p = p->next;
+	while(p != NULL){
+		q = p;
+		p = p->next;
+		free(q);
+	}
+}
+
+void print_for_ll(node *ref){
+	node *p = ref;//, *q = ref;
+	printf("rnll p : %p\n", p);
+	for(node *p = ref; p != NULL; p = p->next){
+		printf("rnll %p %d\n",p, p->num);
+	}
+}
+
+void remove_node(node *ref, int x){
+	node *p = ref;
+	node *q = p->next;
+	if(p == NULL) return;
+	if(p->num == x && q != NULL){
+			// before
+			// { 108, 0x5 } @ 0x4
+			// { 109, 0x6 } @ 0x5
+			// { 110, 0x0 } @ 0x6
+			// ==================
+			// after
+			// { 108, 0x5 } @ 0x4
+			// { 110, 0x0 } @ 0x5
+			// free the previous address of p->next
+
+			node tmp = *p;
+			*p = *(p->next);
+			free(tmp.next);
+	}
+	// last element check
+	else if(q != NULL && q->next == NULL && q->num == x){
+		free(p->next);
+		p->next = NULL;
+		return;
+	}
+	else {
+		p = p->next;
+		remove_node(p, x);
+	}
+}
+
+void testrun(int x){
+	printf("===\n");
+	printf("removing %d\n", x);
+	node base;
+	assign_ll(&base);
+	printf("before\n");
+	print_ll(&base);
+	remove_node(&base, x);
+	printf("after\n");
+	print_ll(&base);
+	free_ll(&base);
+}
+
+int main(){
+	testrun(101);
+	testrun(108);
+	testrun(110);
+}

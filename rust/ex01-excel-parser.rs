18631b5c41d499078ed3419273d68dc5bb744b00
sgpx <spgithubacc@gmail.com>
2024-04-11 23:27:52 +0530

    2024-04-11-23-27

--- rust/ex01-excel-parser.rs
+++ rust/ex01-excel-parser.rs
@@ -5,12 +5,17 @@ use std::{collections::HashMap, env};
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    let filename = args.last().expect("RUSTERROR: invalid args");
-    let mut workbook: Xlsx<_> = open_workbook(filename).expect("RUSTERROR: Cannot open file");
+    let filename = args.last().expect("ERROR: invalid args");
+    let mut workbook: Xlsx<_> = open_workbook(filename).expect("ERROR: Cannot open file");
 
     let mut first_row_passed = false;
     let mut header_map: HashMap<u32, String> = HashMap::new();
-    if let Some(Ok(range)) = workbook.worksheet_range("CODING SHEET") {
+    if let Some(Ok(range)) = workbook.worksheet_range("MYSHEET") {
+        let required_fields: Vec<&str> = vec![
+            "foo",
+            "bar",
+            "baz",
+        ];
         let mut result_vector = vec![];
 
         for row in range.rows() {
@@ -24,16 +29,19 @@ fn main() {
                 }
                 first_row_passed = true;
             } else {
-                if let Some(ix) = row_values.get(0) {
-                    let iy = String::from(ix);
+                if let Some(first_cell) = row_values.get(0) {
+                    let first_cell_string = String::from(first_cell);
                     let mut hv_pair: HashMap<String, String> = HashMap::new();
-                    if !iy.is_empty() {
+                    if !first_cell_string.is_empty() {
                         let mut cell_index = 0;
                         for val in row_values.iter() {
                             let header = header_map
                                 .get(&cell_index)
-                                .expect("RUSTERROR: Some error occurred while getting headers");
-                            hv_pair.insert(header.to_string(), val.to_string());
+                                .expect("ERROR: Some error occurred while getting headers");
+                            let header_ref = header.to_string();
+                            if required_fields.contains(&&header_ref.as_str()) {
+                                hv_pair.insert(header.to_string(), val.to_string());
+                            }
                             cell_index += 1;
                         }
                         if hv_pair.len() > 0 {
@@ -44,9 +52,9 @@ fn main() {
             }
         }
         let jsout: String =
-            serde_json::to_string(&result_vector).expect("RUSTERROR: could not serialize JSON");
+            serde_json::to_string(&result_vector).expect("ERROR: could not serialize JSON");
         println!("{}", jsout);
     } else {
-        println!("RUSTERROR: Sheet named 'CODING SHEET' not found.");
+        println!("ERROR: Sheet named 'MYSHEET' not found.");
     }
 }

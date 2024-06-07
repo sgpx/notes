ee03f1ab2f538c95c10407af4b9ddd5704555f0f
sgpx <spgithubacc@gmail.com>
2024-06-07 21:13:11 +0530

    2024-06-07-21-13

--- aws-ec2/start-instance-by-id.sh
+++ aws-ec2/start-instance-by-id.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 bash get-instance-vpcs.sh
 read inst
-echo stopping instance $inst
+echo startping instance $inst
 aws ec2 start-instances --instance-ids "$inst"
-st=$(aws ec2 describe-instances --instance-ids $inst | jq '.Reservations[0].Instances[0].State.Name')
+st=$(aws ec2 describe-instances --instance-ids $inst | jq -r '.Reservations[0].Instances[0].State.Name')
 
 while [ "$st" != "running" ]; do
 	echo waiting.. $st

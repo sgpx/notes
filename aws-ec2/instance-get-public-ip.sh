bef81b5c7ec30b0c56531ec7bfe36f43caef5843
sgpx <spgithubacc@gmail.com>
2024-06-07 21:23:25 +0530

    2024-06-07-21-23

--- aws-ec2/instance-get-public-ip.sh
+++ aws-ec2/instance-get-public-ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash get-instance-vpcs.sh | grep "$1" | sed -r "s/(.+) # (.+) # (.+)/\2/g" | head -n1
+bash get-public-ips-for-all-instances.sh | grep "$1" | sed -r "s/(.+) # (.+) # (.+)/\2/g" | head -n1

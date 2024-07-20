d9c0d2f88ce6a096ec82f9bad1eb8c6fb909480a
sgpx <spgithubacc@gmail.com>
2024-07-20 18:33:11 +0530

    2024-07-20-18-33

--- ffmpeg/convert-mov-to-mp4.sh
+++ ffmpeg/convert-mov-to-mp4.sh
@@ -2,6 +2,6 @@
 
 # input and output paths
 input_path=$1
-output="output.mp4"
+output_path="output.mp4"
 # the command
 ffmpeg -i "$input_path" -vf "scale=1080:720" -c:v libx264 -crf 23 -c:a aac "$output_path"

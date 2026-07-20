78d423dc9653b3742efccea0f71fc0a03d731fec
sgpx <spgithubacc@gmail.com>
2026-07-20 16:55:40 +0530

    2026-07-20-16-55-40

--- pytorch/ex270.py
+++ pytorch/ex270.py
@@ -1,1 +1,9 @@
 # Write a program to compute the Jacobian matrix of the function f(x)=x^3 evaluated at the tensor x=[2.0,3.0] using torch.autograd.functional.jacobian.
+import torch
+
+f = lambda X : X**3
+
+x = torch.tensor([2.0, 3.0])
+
+
+print(torch.autograd.functional.jacobian(f, x))
